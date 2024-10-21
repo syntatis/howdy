f772a583a2e881ed9a6664bd9f71bc4b0beb0900
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-10-21 18:32:09 +0700

    Add file comment

--- inc/bootstrap/dev.php
+++ inc/bootstrap/dev.php
@@ -1,3 +1,10 @@
 <?php
 
+/**
+ * This file will be autoloaded by the Composer autoloader and it will be
+ * loaded only in the dev environment. Use this file to run classes or
+ * functions to help during development such as that for debugging,
+ * testing or logging.
+ */
+
 declare(strict_types=1);
